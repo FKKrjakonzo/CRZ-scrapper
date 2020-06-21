acfb25b7bb9f760e1d63914593953a555b3b0b23
FKKrjakonzo <fkkrjakonzo@gmail.com>
2020-06-21 20:47:38 +0200

    Add files via upload

--- classes.py
+++ classes.py
@@ -7,84 +7,89 @@ import xlsxwriter
 
 class search_obj:
 
-    def __init__(self, cena, cesta, csv):
-        self.csv = csv
-        self.counter = 0
-        self.copies = 0
-        self.cena_od = cena[0]
-        self.cena_do = cena[1]
-        if(self.csv):
-            self.file_set('{}{}.csv'.format(cesta, datetime.now().strftime('%d_%m_%Y_%H'
-                      )))
-        else:
-            self.file_set('{}{}.xlsx'.format(cesta, datetime.now().strftime('%d_%m_%Y_%H'
-                      )))
+	def __init__(self, cena, cesta, csv):
+		self.csv = csv
+		self.counter = 0
+		self.copies = 0
+		self.cena_od = cena[0]
+		self.cena_do = cena[1]
+		if(self.csv):
+			self.file_set('{}{}.csv'.format(cesta, datetime.now().strftime('%d_%m_%Y_%H_%M'
+					  )))
+		else:
+			self.file_set('{}{}.xlsx'.format(cesta, datetime.now().strftime('%d_%m_%Y_%H_%M'
+					  )))
 
-    def file_set(self, file):
-        self.file = file
-        if not path.exists('{}\\{}'.format(path.dirname(argv[0]),
-                               self.file)):
-            if(self.csv):
-                f = open(self.file, 'a', encoding='utf_8_sig')
-                f.write('Číslo zmluvy;CRZ ID;Objednávateľ;Adresa objednávateľa;IČO objednávateľa;Dodávateľ;Adresa dodávateľa;IČO dodávateľa;Názov zmluvy;Dátum zverejnenia;Dátum uzavretia;Dátum účinnosti;Dátum platnosti do;Zmluvne dohodnutá čiastka;Celková čiastka;'
-                            )
-                f.write('\n')
-                f.close()
-            else:
-                self.workbook =xlsxwriter.Workbook(self.file)
-                self.worksheet = self.workbook.add_worksheet()
-                self.worksheet.write(0, 1, 'Číslo zmluvy')
-                self.worksheet.write(0, 2, 'CRZ ID')
-                self.worksheet.write(0, 3, 'Objednávateľ')
-                self.worksheet.write(0, 4, 'Adresa objednávateľa')
-                self.worksheet.write(0, 5, 'IČO objednávateľa')
-                self.worksheet.write(0, 6, 'Dodávateľ')
-                self.worksheet.write(0, 1, 'Adresa dodávateľa')
-                self.worksheet.write(0, 2, 'IČO dodávateľa')
-                self.worksheet.write(0, 3, 'Názov zmluvy')
-                self.worksheet.write(0, 4, 'Adresa objednávateľa')
-                self.worksheet.write(0, 5, 'Dátum zverejnenia')
-                self.worksheet.write(0, 6, 'Dátum uzavretia')
-                self.worksheet.write(0, 6, 'Dátum účinnosti')
-                self.worksheet.write(0, 6, 'Dátum platnosti do')
-                self.worksheet.write(0, 6, 'Zmluvne dohodnutá čiastka')
-                self.worksheet.write(0, 6, 'Celková čiastka')
-                self.row = 1
-        else:
-            if not self.csv:
-                self.file_set('{}'.format('{}_{}.xlsx'.format(self.file[:-4],
-                              self.copies)))
+	def file_set(self, file):
+		self.file = file
+		if not path.exists('{}\\{}'.format(path.dirname(argv[0]),
+							   self.file)):
+			if(self.csv):
+				f = open(self.file, 'a', encoding='utf_8_sig')
+				f.write('Číslo zmluvy;CRZ ID;Objednávateľ;Adresa objednávateľa;IČO objednávateľa;Dodávateľ;Adresa dodávateľa;IČO dodávateľa;Názov zmluvy;Dátum zverejnenia;Dátum uzavretia;Dátum účinnosti;Dátum platnosti do;Zmluvne dohodnutá čiastka;Celková čiastka;'
+							)
+				f.write('\n')
+				f.close()
+			else:
+				self.workbook =xlsxwriter.Workbook(self.file)
+				self.workbook.close()
+				self.workbook =xlsxwriter.Workbook(self.file)
+				self.worksheet = self.workbook.add_worksheet()
+				self.worksheet.write(0, 0, 'Číslo zmluvy')
+				self.worksheet.write(0, 1, 'CRZ ID')
+				self.worksheet.write(0, 2, 'Objednávateľ')
+				self.worksheet.write(0, 3, 'Adresa objednávateľa')
+				self.worksheet.write(0, 4, 'IČO objednávateľa')
+				self.worksheet.write(0, 5, 'Dodávateľ')
+				self.worksheet.write(0, 6, 'Adresa dodávateľa')
+				self.worksheet.write(0, 7, 'IČO dodávateľa')
+				self.worksheet.write(0, 8, 'Názov zmluvy')
+				self.worksheet.write(0, 9, 'Dátum zverejnenia')
+				self.worksheet.write(0, 10, 'Dátum uzavretia')
+				self.worksheet.write(0, 11, 'Dátum účinnosti')
+				self.worksheet.write(0, 12, 'Dátum platnosti do')
+				self.worksheet.write(0, 13, 'Zmluvne dohodnutá čiastka')
+				self.worksheet.write(0, 14, 'Celková čiastka')
+				self.row = 1
+		else:
+			if not self.csv:
+				self.file_set('{}'.format('{}_{}.xlsx'.format(self.file[:-4],
+							  self.copies)))
 
-    def insert_obj(self, line):
-        self.counter += 1
-        if self.counter >= 200000:
-            if(self.csv):
-                self.file_set('{}'.format('{}_{}.csv'.format(self.file[:-4],
-                              self.copies)))
-            else:
-                self.file_set('{}'.format('{}_{}.xlsx'.format(self.file[:-4],
-                              self.copies)))
-                self.copies += 1
-                self.counter = 0
-                self.row = 0
-                self.close()
-        if(self.csv):
-            with open(self.file, 'a', encoding='utf_8_sig') as f:
-                for i in line:
-                    f.write('{};'.format(i))
-                f.write('\n')
-        else:
-            for i in range(len(line)):
-                self.worksheet.write(self.row, i, line[i])  
-            self.row += 1
-    def close(self):
-        self.workbook.close()
+	def insert_obj(self, line):
+		self.counter += 1
+		if self.counter >= 200000:
+			if(self.csv):
+				self.file_set('{}'.format('{}_{}.csv'.format(self.file[:-4],
+							  self.copies)))
+			else:
+				self.file_set('{}'.format('{}_{}.xlsx'.format(self.file[:-4],
+							  self.copies)))
+				self.copies += 1
+				self.counter = 0
+				self.row = 0
+				self.close()
+		if(self.csv):
+			with open(self.file, 'a', encoding='utf_8_sig') as f:
+				for i in line:
+					f.write('{};'.format(i))
+				f.write('\n')
+		else:
+			for i in range(len(line)):
+				self.worksheet.write(self.row, i, line[i])  
+			self.row += 1
 
-    def filter_lines(self, line):
-        if self.cena_od:
-            if float(line[14]) < self.cena_od:
-                return False
-        if self.cena_do:
-            if float(line[14]) > self.cena_do:
-                return False
-        self.insert_obj(line)
+	def close(self):
+		try:
+			self.workbook.close()
+		except AttributeError:
+			pass
+
+	def filter_lines(self, line):
+		if self.cena_od:
+			if float(line[14]) < self.cena_od:
+				return False
+		if self.cena_do:
+			if float(line[14]) > self.cena_do:
+				return False
+		self.insert_obj(line)
--- main.py
+++ main.py
@@ -1,156 +1,184 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import threading
 from io import BytesIO
 from zipfile import ZipFile
+from urllib.error import URLError
 from urllib.request import urlopen
-from untangle import parse
 from classes import search_obj
 from datetime import datetime, timedelta
 from tkinter import Button, Label, Entry, Tk, HORIZONTAL, W, messagebox, filedialog, IntVar, Radiobutton
 from tkcalendar import DateEntry
 from tkinter.ttk import Progressbar
 from time import sleep, time
-
-root = Tk()
-root.geometry('280x250')
-root.title('Božský CRZ scrapper')
-now = datetime.now()
-ready = True
-
-cal = DateEntry(
-    root,
-    width=12,
-    year=now.year,
-    month=now.month,
-    day=now.day,
-    background='darkblue',
-    foreground='white',
-    borderwidth=3,
-    )
-cal.place(x=70, y=10)
-
-cal1 = DateEntry(
-    root,
-    width=12,
-    year=now.year,
-    month=now.month,
-    day=now.day,
-    background='darkblue',
-    foreground='white',
-    borderwidth=3,
-    )
-cal1.place(x=70, y=40)
-
-label_1 = Label(root, text='Dátum od:').place(x=0, y=10)
-label_2 = Label(root, text='Dátum do:').place(x=0, y=40)
-label_3 = Label(root, text='Cena od: ').place(x=0, y=70)
-label_4 = Label(root, text='Cena do: ').place(x=0, y=100)
-
-e1 = Entry(root, width=15)
-e1.grid(row=2, column=1, sticky=W)
-e1.place(x=70, y=70)
-e1.insert(0, '0')
-
-#
-
-e2 = Entry(root, width=15)
-e2.grid(row=2, column=1, sticky=W)
-e2.place(x=70, y=100)
-e2.insert(0, '0')
-progress = Progressbar(root, orient=HORIZONTAL, length=190,
-                       mode='determinate')
+import xml.etree.ElementTree as ET
+from lxml import etree
 
 
-def bar():
-
-    date_od = datetime.strptime(cal.get(), '%m/%d/%y')
-    date_do = datetime.strptime(cal1.get(), '%m/%d/%y')
-
-    if date_do < date_od:
-        messagebox.showerror('showerror',
-                             'Dátum od je väčší ako do.'
-                             )
-        return 0
-
-    try:
-        value = int(e1.get())
-    except ValueError:
-        try:
-            value = float(e1.get())
-        except ValueError:
+class class_one:
+    def main_scikit(self):
+        date_od = datetime.strptime(self.cal.get(), '%m/%d/%y')
+        date_do = datetime.strptime(self.cal1.get(), '%m/%d/%y')
+        if date_do < date_od:
             messagebox.showerror('showerror',
-                                 'Cena od nie je číslo, je potrebné oddeľovať hodnoty s .'
+                                 'Dátum od je väčší ako do.'
                                  )
             return 0
-    try:
-        value1 = int(e2.get())
-    except ValueError:
+
         try:
-            value1 = float(e2.get())
+            value = int(self.e1.get())
         except ValueError:
-            messagebox.showerror('showerror',
-                                 'Cena do nie je číslo, je potrebné oddeľovať hodnoty s .'
-                                 )
-            return 0
-    if(v.get() == 1):
-        csv = True
-    else:
-        csv = False
-    if(button_1["text"] != "Vlastná cesta"):
-        Objekt = search_obj([value, value1], "{}\\".format(button_1["text"]), csv)
-    else:
-        Objekt = search_obj([value, value1], "", csv)
-    progress.place(x=5, y=170)
-    steps = (date_do - date_od).days
-    done = 0
+            try:
+                value = float(self.e1.get())
+            except ValueError:
+                messagebox.showerror('showerror',
+                                     'Cena od nie je číslo, je potrebné oddeľovať hodnoty s .'
+                                     )
+                return 0
+        try:
+            value1 = int(self.e2.get())
+        except ValueError:
+            try:
+                value1 = float(self.e2.get())
+            except ValueError:
+                messagebox.showerror('showerror',
+                                     'Cena do nie je číslo, je potrebné oddeľovať hodnoty s .'
+                                     )
+                return 0
+        if(self.v.get() == 1):
+            csv = True
+        else:
+            csv = False
+        if(self.button_1["text"] != "Vlastná cesta"):
+            Objekt = search_obj([value, value1], "{}\\".format(self.button_1["text"]), csv)
+        else:
+            Objekt = search_obj([value, value1], "", csv)
+        
+        steps = (date_do - date_od).days
+        done = 0
+        self.progress.place(x=5, y=170)
+        left_time = time()
+        while date_od != date_do:
+            start_time = time()
+            date_value = '{}-{:02d}-{:02d}'.format(date_od.year,
+                    date_od.month, date_od.day)
+            url = 'https://www.crz.gov.sk/export/{}.zip'.format(date_value)
+            try:
+                resp = urlopen(url)             
+                zipfile = ZipFile(BytesIO(resp.read()))
+                parser = etree.XMLParser(recover=True, encoding = 'utf-8')
 
-    while date_od != date_do:
-        start_time = time()
-        progress['value'] = done * 100 / steps
-        date_value = '{}-{:02d}-{:02d}'.format(date_od.year,
-                date_od.month, date_od.day)
-        url = 'https://www.crz.gov.sk/export/{}.zip'.format(date_value)
-        resp = urlopen(url)
-        zipfile = ZipFile(BytesIO(resp.read()))
-        obj = parse(zipfile.open('{}.xml'.format(date_value)))
-        for i in obj.zmluvy.zmluva:
-            Objekt.filter_lines([
-                i.nazov.cdata,
-                i.ID.cdata,
-                i.zs1.cdata,
-                i.sidlo1.cdata,
-                i.ico1.cdata,
-                i.zs2.cdata,
-                i.sidlo.cdata,
-                i.ico.cdata,
-                i.predmet.cdata,
-                i.datum_zverejnene.cdata,
-                i.datum.cdata,
-                i.datum_ucinnost.cdata,
-                i.datum_platnost_do.cdata,
-                i.suma_zmluva.cdata,
-                i.suma_spolu.cdata,
-                ])
+                tree = ET.parse(zipfile.open('{}.xml'.format(date_value)), parser=parser)
+                obj = tree.getroot()
+                for i in obj:
+                        Objekt.filter_lines([
+                            i[0].text,
+                            i[1].text,
+                            i[2].text,
+                            i[21].text,
+                            i[20].text,
+                            i[3].text,
+                            i[19].text,
+                            i[13].text,
+                            i[4].text,
+                            i[12].text,
+                            i[24].text,
+                            i[5].text,
+                            i[6].text,
+                            i[7].text,
+                            i[8].text,
+                            ])
+                done += 1
+                m, s = divmod(int(((time()-left_time)/done)*steps-(time()-left_time)), 60)
+                h, m = divmod(m, 60)
+                self.label_5['text']= "{:02d}:{:02d}:{:02d} ostáva".format(h, m, s)
+                self.progress['value'] = done * 100 / steps
+                sleep(0.02)
+                self.root.update_idletasks()
+                date_od = date_od + timedelta(days=1)
+                if time() - start_time < 3:
+                    sleep(2)
+            except URLError:
+                sleep(2)
+        self.progress['value'] = 100
+        Objekt.close()
 
-        done += 1
-        date_od = date_od + timedelta(days=1)
-        if time() - start_time < 3:
-            sleep(2)
-    progress['value'] = 100
-    Objekt.close()
+    def pick_path(self):
+        folder_selected = filedialog.askdirectory()
+        if(folder_selected):
+            self.button_1["text"] = folder_selected
 
-def pick_path():
-    folder_selected = filedialog.askdirectory()
-    if(folder_selected):
-        button_1["text"] = folder_selected
+    def main_form(self):
 
-v = IntVar() 
-Radiobutton(root, text="CSV", variable=v, value=1).place(x=180, y=10)
-Radiobutton(root, text="XLSX", variable=v, value=2).place(x=180, y=30)
-v.set(1)
-button_1 = Button(root, text="Vlastná cesta", command=pick_path)
-button_1.place(x=5, y=130)
+        self.root = Tk()
+        self.root.geometry('300x250')
+        self.root.title('CRZ scrapper')
+        now = datetime.now()
+        ready = True
 
-button = Button(root, text='Štart', command=bar).place(x=80,
-        y=210)
-root.mainloop()
+        self.cal = DateEntry(
+            self.root,
+            width=12,
+            year=now.year,
+            month=now.month,
+            day=now.day,
+            background='darkblue',
+            foreground='white',
+            borderwidth=3,
+            )
+        self.cal.place(x=70, y=10)
+
+        self.cal1 = DateEntry(
+            self.root,
+            width=12,
+            year=now.year,
+            month=now.month,
+            day=now.day,
+            background='darkblue',
+            foreground='white',
+            borderwidth=3,
+            )
+        self.cal1.place(x=70, y=40)
+
+        label_1 = Label(self.root, text='Dátum od:').place(x=0, y=10)
+        label_2 = Label(self.root, text='Dátum do:').place(x=0, y=40)
+        label_3 = Label(self.root, text='Cena od: ').place(x=0, y=70)
+        label_4 = Label(self.root, text='Cena do: ').place(x=0, y=100)
+
+        self.e1 = Entry(self.root, width=15)
+        self.e1.grid(row=2, column=1, sticky=W)
+        self.e1.place(x=70, y=70)
+        self.e1.insert(0, '0')
+
+        #
+
+        self.e2 = Entry(self.root, width=15)
+        self.e2.grid(row=2, column=1, sticky=W)
+        self.e2.place(x=70, y=100)
+        self.e2.insert(0, '0')
+        self.progress = Progressbar(self.root, orient=HORIZONTAL, length=190,
+                               mode='determinate')
+        self.v = IntVar() 
+        Radiobutton(self.root, text="CSV", variable=self.v, value=1).place(x=180, y=10)
+        Radiobutton(self.root, text="XLSX", variable=self.v, value=2).place(x=180, y=30)
+        self.v.set(1)
+        self.button_1 = Button(self.root, text="Vlastná cesta", command=self.pick_path)
+        self.button_1.place(x=5, y=130)
+
+        button = Button(self.root, text='Štart', command=self.start_thread).place(x=80,
+                y=210)
+
+        self.label_5 = Label(self.root, text = " ")
+        self.label_5.place(x=200, y=170)
+
+        label_6 = Label(self.root, text = "0 = neobmedzene")
+        label_6.place(x=175, y=80)
+        self.root.mainloop()
+
+    def start_thread(self):
+        maincal = threading.Thread(target=self.main_scikit)
+        maincal.start()
+
+
+
+co = class_one()
+mainfz = threading.Thread(target=co.main_form)
+mainfz.start() 
